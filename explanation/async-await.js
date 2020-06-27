88e275b263a23d56ecc41b0bdf4c19e489d85718
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2020-06-27 18:52:45 -0500

    Mis notas en Async Await :heart:

--- explanation/async-await.js
+++ explanation/async-await.js
@@ -1,3 +1,7 @@
+// Siempre se requiere async antes de function
+// Await es el que indica que el proceso es asincrono
+// Es mas amigable, siempre los await hacen que esperen para la siguiente linea.
+// Los errores se pueden manejar con los try catch
 async function requestHandler(req, res) {
   try {
     const user = await User.findById(req.userId);
